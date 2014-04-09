f4090ce117997fbe430f3e95ca9d0cca2098b221
Chris Granger <ibdknox@gmail.com>
2014-04-09 00:29:01 -0700

    import
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

---
+++ js/default.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $("#download").click(function() {
+    $("#download").toggle();
+  });
+
+  $(".download").click(function() {
+    $("#download").toggle();
+  });
+
+});
